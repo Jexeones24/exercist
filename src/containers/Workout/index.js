97f0b227fc99678facf6ba301f0b2f88c95016d5
Jexeones24 <jexeones@gmail.com>
2018-09-21 00:26:04 -0400

    Renders workout after time domain collected

--- src/containers/Workout/index.js
+++ src/containers/Workout/index.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 import { buildWorkout } from './util';
+import styles from './workout.module.scss';
 
 class Workout extends Component {
-  workout = () => buildWorkout(this.props.time);
+  get workout() {
+    return buildWorkout(this.props.time);
+  }
+
+  get title() {
+    return this.workout.title;
+  }
+
+  get reps() {
+    return (
+      this.workout.formattedRepsAndMovements
+      .map((reps, idx) => (
+        <div key={idx}>{ reps }</div>
+      ))
+    );
+  }
 
   render () {
-    const workout = this.workout();
-    console.log(workout);
-
     return (
-      <div>
-        HELLO
+      <div className={styles['workout-container']}>
+        <h1>{ this.title }</h1>
+        <div>{ this.reps }</div>
       </div>
     );
   }
